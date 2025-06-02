cb977c0115fb56ec23f57349adffa836e571a597
keval Jadhav <kevaljadhav87@gmail.com>
2025-06-02 11:43:20 +0000

    learning about JS datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";// treat all JS code as newer version
+
+//alert("Hello There ")// we are using node not browser for code thats why we dont using alert 
+
+let name = "hitesh"
+let age = 23
+let isLoggedIn = false 
+
+
+console.table([name,age,isLoggedIn]);
+
+
+
+//number => 2 to power 53 (range of number just note)
+// bigInt 
+// string => ""
+// boolean => ture/false
+// null  => standalone value
+// undefined => 
+//symbol => unique
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
