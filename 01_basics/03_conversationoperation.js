35528d1119cc80239449cbb66c5f28535850affc
keval Jadhav <kevaljadhav87@gmail.com>
2025-06-03 10:55:50 +0000

    learning about operations in javascript

--- 01_basics/03_conversationoperation.js
+++ 01_basics/03_conversationoperation.js
@@ -1,35 +1,71 @@
-/*let score = null
+let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valueInNumber = Number(score)
 
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
-*/
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN(not a number)
 // true => 1; false => 0
 
 
-/* let isLoggedIn = "Hitesh"
+let isLoggedIn = "Keval"
 
  let booleanIsLoggedIn =Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn);
-*/
+//  console.log(booleanIsLoggedIn);
+
 
 
 // 1 => true;
 // 0 => false
 // "" => false
-// "hitesh" => true
+// "Keval" => true
 
 let someNumber = 33
 
 let stringNumber = String(someNumber )
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+    // *************************** Operations *******************************
+
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2/2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+
+let str1 = "Hello"
+let str2 = " Keval"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
