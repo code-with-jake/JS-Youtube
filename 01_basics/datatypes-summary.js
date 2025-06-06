33067c4f94d6a1801439bf0af125efb3ff162503
keval Jadhav <kevaljadhav87@gmail.com>
2025-06-06 11:27:16 +0000

    Datatypes Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// primitive
+
+// 7 types : String , Number , Boolearn , null , undefined , Symbol , BigInt
+
+
+const score = 100
+const scoreValue = 100.69
+
+const isLoggedIn = False 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 7456723461923785n
+
+// Reference (non primitive)
+
+// array , objects , Functions
+
+
+const heros = ["iron man ","thor","hulk"];
+let myObj ={
+    name : "keval jadhav",
+    age :20, 
+}
+
+const myfunction = function () {
+    console.log("Hello motherfucker");
+    
+}
+
+console.log(typeof bigNumber);
+
