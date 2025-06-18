835b33eaf78549871b5c20e57134a34408a9fbca
keval Jadhav <kevaljadhav87@gmail.com>
2025-06-18 08:54:08 +0000

    Learninga about Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+const name = "keval jadhav "
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`hello My name is ${name} and my repo count is ${repoCount}`);
+
+
+const gamename = new String('keval jadhav')
+
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -3,7 +3,7 @@
 // 7 types : String , Number , Boolearn , null , undefined , Symbol , BigInt
 
 
-const score = 100
+/*const score = 100
 const scoreValue = 100.69
 
 const isLoggedIn = False 
@@ -12,16 +12,16 @@ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-
+*/
 // console.log(id === anotherId);
 
-const bigNumber = 7456723461923785n
+// const bigNumber = 7456723461923785n
 
 // Reference (non primitive)
 
 // array , objects , Functions
 
-
+/*
 const heros = ["iron man ","thor","hulk"];
 let myObj ={
     name : "keval jadhav",
@@ -29,9 +29,38 @@ let myObj ={
 }
 
 const myfunction = function () {
-    console.log("Hello motherfucker");
+    // console.log("Hello motherfucker");
     
 }
+*/
+// console.log(typeof bigNumber);
 
-console.log(typeof bigNumber);
 
+
+// *****************************************
+
+
+// Stack  (Primitive), Heap (Non - Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let  anothername = myYoutubename
+anothername = "chaiorbhok"
+
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+
+let userOne ={
+    email : "keval@gmail.com",
+    upi : "diko@bob"
+}
+
+
+let usertwo = userOne
+
+usertwo.email = "mohan@gmail.com"
+
+console.log(userOne.email);
+console.log(usertwo.email);
